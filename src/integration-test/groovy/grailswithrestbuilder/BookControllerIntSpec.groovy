d9654d18bf9205f4e4802030f3b3f5af608b5a11
niravassar <assarn@objectcomputing.com>
2022-01-24 16:37:46 -0600

    functional test folder set up with gradle

--- src/integration-test/groovy/grailswithrestbuilder/BookControllerIntSpec.groovy
+++ src/integration-test/groovy/grailswithrestbuilder/BookControllerIntSpec.groovy
@@ -26,29 +26,29 @@ class BookControllerIntSpec extends Specification {
         //new Book(title: "RobBook").save(failOnError: true, flush: true)
     }
 
-    void "test http call with restbuilder"() {
-        when:"books are called"
-        RestResponse resp = rest.get("http://localhost:${serverPort}/books")
-
-        then: "check the logging events"
-        resp.status == 200
-        resp.responseEntity.body.toString().contains("Nirav")
-    }
-
-    void "test logical-delete"() {
-        when:
-        Book niravBook = Book.findByTitle('NiravBook')
-        niravBook.delete(something: 1)
-        List<Book> books = Book.list()
-        def whereQuery = Book.where {
-            title == "NiravBook" || title == "RobBook"
-        }.list()
-
-        then:
-        // these values should fail bc the query is not considering logically deleted items.
-        assert books.size() == 2 // SHOULD BE 1
-        assert whereQuery.size() == 1 // SHOULD BE 0
-    }
+//    void "test http call with restbuilder"() {
+//        when:"books are called"
+//        RestResponse resp = rest.get("http://localhost:${serverPort}/books")
+//
+//        then: "check the logging events"
+//        resp.status == 200
+//        resp.responseEntity.body.toString().contains("Nirav")
+//    }
+//
+//    void "test logical-delete"() {
+//        when:
+//        Book niravBook = Book.findByTitle('NiravBook')
+//        niravBook.delete(something: 1)
+//        List<Book> books = Book.list()
+//        def whereQuery = Book.where {
+//            title == "NiravBook" || title == "RobBook"
+//        }.list()
+//
+//        then:
+//        // these values should fail bc the query is not considering logically deleted items.
+//        assert books.size() == 2 // SHOULD BE 1
+//        assert whereQuery.size() == 1 // SHOULD BE 0
+//    }
 
     void "test grails GrailsDomainBinder"() {
         when:
